7d44d31fc13101d32d6472f9c58e358c99639baf
AnastasiaU <anastasia.trembach@gmail.com>
2020-01-02 13:32:09 +0000

    labs are done

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  //if (typeof obj !== 'object') return;
+  for (const key in obj) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+};
+
+const obj = { a: 1, b: 2, c: 3 };
+iterate(obj, (key, value) => {
+  console.log({ key, value });
+});
+
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,36 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (x, y) => {
+  if (!types[0](x)) throw Error('TypeError: wrong first argument');
+
+  if (!types[1](y)) throw Error('TypeError: wrong second argument');
+
+  if (typeof types[2](fn(x, y)) !== typeof fn(x, y)) throw Error('TypeError: wrong result of function(third argument)');
+
+  return fn(x, y);
+};
+
+{
+  const add = (a, b) => a + b;
+  const addNumbers = contract(add, Number, Number, Number);
+  const res = addNumbers(2, 3);
+  console.dir(res);
+}
+
+
+
+{
+  const concat = (s1, s2) => s1 + s2;
+  const concatStrings = contract(concat, String, String, String);
+  const res = concatStrings('Hello ', 'world!');
+  console.dir(res);
+}
+
+// const s = 6;
+// const q = 5 + '';
+// const type = Number(s+q);
+// if (type) console.log('=');
+// if (!NaN) console.log('=');
+// console.log(typeof s+q == typeof type);
 
 module.exports = { contract };
